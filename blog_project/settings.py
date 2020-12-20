14bac004c31e1ebcbd6644a2e1846bf2f39d1522
mariya754 <mpismennaya@gmail.com>
2020-12-20 19:37:23 +0200

    Adding accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDEIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
